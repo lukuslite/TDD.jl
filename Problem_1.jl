0816f1bcaad72dc87947af43176ca7567ae67a1c
Luke Long <l.luke@wustl.edu>
2023-03-02 17:06:47 -0600

    Added Function to satisfy tests

--- Problem_1.jl
+++ Problem_1.jl
@@ -1,7 +1,16 @@
 using Test
 
-@test Connected(graph = [[2,3],[1],[1],[5],[4]],node = 1) == [1,2,3]
-@test Connected(graph = [[2,3],[1],[1],[5],[4]],node = 3) == [3,1]
-@test Connected(graph = [[2,3],[1],[1],[5],[4]],node = 4) == [4,5]
-@test Connected(graph = [[4,5,7],[3],[2,5],[6],[12]],node = 1) == [1,4,5,7]
-@test Connected(graph = [[4,5,7],[3],[2,5],[6],[12]],node = 5) == [5,12]
+function Connected(graph::Vector{Vector{Int64}},node::Int64)
+    connected_nodes = Vector{Int64}()
+    push!(connected_nodes,node)
+    for i in 1:length(graph[node])
+        push!(connected_nodes,graph[node][i])
+    end
+    return connected_nodes
+end
+
+@test Connected([[2,3],[1],[1],[5],[4]],1) == [1,2,3]
+@test Connected([[2,3],[1],[1],[5],[4]],3) == [3,1]
+@test Connected([[2,3],[1],[1],[5],[4]],4) == [4,5]
+@test Connected([[4,5,7],[3],[2,5],[6],[12]],1) == [1,4,5,7]
+@test Connected([[4,5,7],[3],[2,5],[6],[12]],5) == [5,12]
